da617d043d1771f67db8d830a5fecf0e9a61a277
PC <PC@DESKTOP-PH41RT2>
2021-06-18 15:47:13 +0200

    add class in model

---
+++ src/java/fr/solutec/model/Conseiller.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.solutec.model;
+
+/**
+ *
+ * @author PC
+ */
+public class Conseiller extends Person{
+
+    public Conseiller(int id, String nom, String prenom, String login, String password) {
+        super(id, nom, prenom, login, password);
+    }
+
+    public Conseiller() {
+    }
+    
+}
