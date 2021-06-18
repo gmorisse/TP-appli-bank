e92384816f6004fb1841d60c3077ae5cbce1dcd6
Pierre <Pierre@LAPTOP-PAUKTVEB>
2021-06-18 16:23:06 +0200

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/java/fr/solutec/dao/AccessBD.java
+++ src/java/fr/solutec/dao/AccessBD.java
@@ -14,7 +14,7 @@ import java.sql.*;
 public class AccessBD {
     
     private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-    private static final String DB_NAME = "debut_bis";
+    private static final String DB_NAME = "tp_banque";
     private static final String URL = "jdbc:mysql://localhost/" + DB_NAME;
     private static final String USER = "root";
     private static final String PASSWORD = "";
